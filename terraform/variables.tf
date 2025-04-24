79fa667b2d8b9c6a960f3837f9ce98f23e28e5c4
Amarachi Iheanacho <amarachiiheanacho@Amarachis-MacBook-Pro.local>
2025-04-24 08:52:42 +0100

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "ami_name_filter" {
+  default = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
+}
+
+variable "allowed_ssh_cidr" {
+  default = "197.210.29.237/32"
+}
+
+variable "key_name" {
+  default = "jumphost_key"
+}
+
+variable "public_key" {
+  description = "Your SSH public key"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQ/Nlafmdo1ozq60DGns8Ri37Vxm2mH+HiUnf+QfyZ8 amarachiiheanacho@Amarachis-MacBook-Pro.local"
+}
+
+variable "environment" {
+  default = "DevOpsProject"
+}
+
+variable "owner" {
+  default = "Amarachi"
+}
